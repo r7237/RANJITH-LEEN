19d4897016050b2b04b48d9357a4c208498a0b54
r7237 <35914754+r7237@users.noreply.github.com>
2018-03-07 15:03:13 +0530

    Update 4444.c

--- 4444.c
+++ 4444.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
- 
+ #includ<conio.h>
 int main() {
-   double radius, circumference, area; // Declare 3 floating-point variables
-   double pi = 3.14159265;             // Declare and define PI
+   double radius, circumference, area; 
+   double pi = 3.14159265;            
  
-   printf("Enter the radius: ");  // Prompting message
-   scanf("%lf", &radius);         // Read input into variable radius
- 
-   // Compute area and circumference
+   printf("Enter the radius: ");  
+   scanf("%lf", &radius);       
    area = radius * radius * pi;
    circumference = 2.0 * radius * pi;
- 
-   // Print the results
    printf("The radius is %lf.\n", radius);
    printf("The area is %lf.\n", area);
    printf("The circumference is %lf.\n", circumference);
