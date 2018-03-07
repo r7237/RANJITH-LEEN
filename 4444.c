526e6e3fe907c3b56c695840eb5f41a3ba78bea6
r7237 <35914754+r7237@users.noreply.github.com>
2018-03-07 15:01:25 +0530

    Create 4444.c

---
+++ 4444.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+   double radius, circumference, area; // Declare 3 floating-point variables
+   double pi = 3.14159265;             // Declare and define PI
+ 
+   printf("Enter the radius: ");  // Prompting message
+   scanf("%lf", &radius);         // Read input into variable radius
+ 
+   // Compute area and circumference
+   area = radius * radius * pi;
+   circumference = 2.0 * radius * pi;
+ 
+   // Print the results
+   printf("The radius is %lf.\n", radius);
+   printf("The area is %lf.\n", area);
+   printf("The circumference is %lf.\n", circumference);
+ 
+   return 0;
+}
